1e3af3bed3181413bb3015ef8bada30b0198e0be
VladimirDanil <vladimir.danil25@mail.ru>
2021-07-28 08:21:21 -0700

    some update

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
 import axios from 'axios'
-   
+
 Vue.use(axios);
 Vue.config.productionTip = false
 
