7d250768790c8cfe467e9dea2a8538c8dc3b9f4f
SuperDev348 <vladimir.danil25@mail.ru>
2022-04-14 04:53:24 -0800

    some fixing

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
-import axios from 'axios'
 
+Vue.use(axios);
 Vue.config.productionTip = false
 
 new Vue({
