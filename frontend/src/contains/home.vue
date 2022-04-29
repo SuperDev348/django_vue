2ae1fe15b764e3c2e2e6fdccefb6324561506395
SuperDev348 <vladimir.danil25@mail.ru>
2022-04-29 11:27:07 -0800

    update

--- frontend/src/contains/home.vue
+++ frontend/src/contains/home.vue
@@ -18,19 +18,6 @@
                     required
                 ></v-text-field>
             </validation-provider>
-            <validation-provider
-                v-slot="{ errors }"
-                rules="required|max:500"
-                name="remark"
-            >
-                <v-textarea
-                v-model="remark"
-                :counter="500"
-                :error-messages="errors"
-                label="Remark"
-                required
-                ></v-textarea>
-            </validation-provider>
 
             <v-btn
                 class="mr-4"
