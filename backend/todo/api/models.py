75d091a0637f91aaaee6274c1b9516f9dbfce8ae
SuperDev348 <vladimir.danil25@mail.ru>
2024-01-08 09:31:51 +0100

    fix

--- backend/todo/api/models.py
+++ backend/todo/api/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 class Item(models.Model):
     title = models.CharField(max_length=50)
     remark = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     # def __str__(self):
